6e5c24c2a1f1e9f29f2e01ec0519d08c35990e80
Amit gupta <billamitgupta@gmail.com>
2024-02-01 10:07:02 +0530

    login page intergration

--- medisell/src/component/Aboutus.jsx
+++ medisell/src/component/Aboutus.jsx
@@ -4,10 +4,10 @@ import { border } from '@mui/system'
 
 export default function Aboutus() {
   return (
-    <div className='back'style={{ backgroundImage: `url(${image1})`, opacity:1, height:1000} }>
-        <div style={{color:'white' , paddingTop:250}}>
-      <div className='aboutusdiv' style={{height:300, width:530, marginLeft:500,   border:'2px ', boxShadow:"0px 0px 10px rgba(0, 0, 0, 0.5)", borderRadius:"1vh"}}>
-      <p class="mb-4 font-serif text-xl" style={{padding:"1vh"}} >Welcome to Your Pharmacy, your trusted source for high-quality medicines and healthcare products. We are dedicated to serving our community with care and professionalism. Our team of experienced pharmacists is committed to providing expert advice and exceptional service.</p>
+    <div className='back'style={{ backgroundImage: `url(${image1})`, } }>
+        <div style={{color:'white' , padding:"45vh"}}>
+      <div className='aboutusdiv' style={{height:"50vh", width:"100vh", marginLeft:"",   border:'2px ', boxShadow:"0px 0px 10px rgba(0, 0, 0, 0.5)", borderRadius:"1vh"}}>
+      <p className="mb-4 font-serif text-xl" style={{padding:"1vh"}} >Welcome to Your Pharmacy, your trusted source for high-quality medicines and healthcare products. We are dedicated to serving our community with care and professionalism. Our team of experienced pharmacists is committed to providing expert advice and exceptional service.</p>
      <p style={{padding:"1vh"}}>At Your Pharmacy, your health is our top priority. We strive to make a positive impact on the well-being of our customers by offering a wide range of products and valuable health information.</p>
      </div>
       </div>
---
+++ medisell/src/component/Login.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import logi from '/Users/shekhargupta/Desktop/medi_sell/medisell/src/image/e371b4da-02a5-403c-8ca7-59b217a10216.jpeg'
+import Grid from "@mui/material/Unstable_Grid2";
+import '../style/login.css'
+
+function Login() {
+  return (
+   
+    <body>
+        
+    
+    <div className='loginBody'>
+    <div style={{width:"110vh", padding:"2vh", border:"2px solid",borderRadius:"2vh",background: "radial-gradient(243.55% 153.69% at 23.48% -1.07%, #EBF3F5 10.46%, #C5E2F0 100%)"}}>
+        <Grid container spacing={1}>
+        <div >
+            <img src={logi} style={{width:"55vh",height:"60vh", border:" solid",borderRadius:"2vh"}} alt='pic'></img>
+        </div>
+    <div >
+        <form action="" style={{padding:"3vh"}}>
+            <label htmlFor="username" style={{padding:'1vh'}}>Username 
+            <br />
+            <input type="textbox" placeholder='Username' style={{border:"1px ", margin:"1vh" , borderRadius:"2vh", background:"#B4D8E4" ,height:"5vh",width:"40vh"}}/></label>
+            <br />
+
+            <label htmlFor="Password" style={{padding:'1vh'}}>Password 
+            <br />
+            <input type="password" placeholder='Password' style={{border:"1px", margin:"1vh", borderRadius:"2vh", background:"#B4D8E4",height:"5vh",width:"40vh"}}/></label>
+            <br />
+            <label style={{marginTop:"6vh" ,color:'#86C5DA',marginLeft:"2vh"}}>
+                <input type="checkbox" name="checkbox" id=""style={{marginTop:"6vh" ,color:'#86C5DA'}}/>
+                Accept T&C (read document carefully)
+            </label>
+            <br />
+            <input type="submit" value="submit"  style={{background:"blue", borderRadius:"1vh", width:"15vh",height:"6vh",color:"white",marginLeft:"2vh",marginTop:"8vh"}}/>
+            <input type="button" value="" style={{marginLeft:"5vh"}}/>Signin
+        </form>
+    </div>
+    </Grid>
+    </div>
+    </div>
+    </body>
+  )
+}
+
+export default Login
